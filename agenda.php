bbb13831e0024741a0b4b1959bd15e681ea5423a
MikeGlz23 <utm21040106@utma.edu.mx>
2023-11-13 16:50:38 -0600

    agenda

---
+++ agenda.php
@@ -0,0 +1,116 @@
+<?php
+session_start();
+require 'dbcon.php';
+
+//Verificar si existe una sesión activa y los valores de usuario y contraseña están establecidos
+if (isset($_SESSION['username'])) {
+    $username = $_SESSION['username'];
+
+    // Consultar la base de datos para verificar si los valores coinciden con algún registro en la tabla de usuarios
+    $query = "SELECT * FROM user WHERE username = '$username'";
+    $result = mysqli_query($con, $query);
+
+    // Si se encuentra un registro coincidente, el usuario está autorizado
+    if (mysqli_num_rows($result) > 0) {
+        // El usuario está autorizado, se puede acceder al contenido
+    } else {
+        // Redirigir al usuario a una página de inicio de sesión
+        header('Location: login.php');
+        exit(); // Finalizar el script después de la redirección
+    }
+} else {
+    // Redirigir al usuario a una página de inicio de sesión si no hay una sesión activa
+    header('Location: login.php');
+    exit(); // Finalizar el script después de la redirección
+}
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/styleintranet.css">
+    <title>Forma unica registros | Capullitos Ags</title>
+</head>
+<body class="sb-nav-fixed">
+    <?php include 'sidenav.php'; ?>
+    <div id="layoutSidenav">
+        <div id="layoutSidenav_content">
+            <div class="container-fluid">
+                <?php include('message.php'); ?>
+                <div class="row mb-5 mt-5">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4>BASE DE DATOS DE FORMA UNICA DE IDENTIFICACIÓN Y EMERGENCIA
+                                    <a href="formaunicadeidentificacion.php" class="btn btn-primary float-end">Crear Forma</a>
+                                </h4>
+                            </div>
+                            <div class="card-body">
+
+                                <table class="table table-bordered table-striped" style="width: 100%;">
+                                    <thead>
+                                        <tr>
+                                            <th>Id</th>
+                                            <th>Entrada</th>
+                                            <th>Salida</th>
+                                            <th>Dia</th>
+                                            <th>Costo</th>
+                                            <th>Horas</th>
+                                            <th>Email</th>
+                                            <th>Asociado</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                        $query = "SELECT * FROM agendadaycare";
+                                        $query_run = mysqli_query($con, $query);
+
+                                        if (mysqli_num_rows($query_run) > 0) {
+                                            foreach ($query_run as $student) {
+                                        ?>
+                                                <tr>
+                                                    <td><?= $student['id']; ?></td>
+                                                    <td><?= $student['entrada']; ?></td>
+                                                    <td><?= $student['salida']; ?></td>
+                                                    <td><?= $student['dia']; ?></td>
+                                                    <td><?= $student['costo']; ?></td>
+                                                    <td><?= $student['horas']; ?></td>
+                                                    <td><?= $student['email']; ?></td>
+                                                    <td><?= $student['asociado']; ?></td>
+                                                    <td>
+                                                        <a href="verformaunica.php?id=<?= $student['id']; ?>" class="btn btn-info btn-sm">Ver</a>
+                                                        <a href="editarformaunica.php?id=<?= $student['id']; ?>" class="btn btn-success btn-sm">Editar</a>
+                                                        <form action="code.php" method="POST" class="d-inline">
+                                                            <button type="submit" name="delete_student" value="<?= $student['id']; ?>" class="btn btn-danger btn-sm">Borrar</button>
+                                                        </form>
+                                                    </td>
+                                                </tr>
+                                        <?php
+                                            }
+                                        } else {
+                                            echo "<h5> No Record Found </h5>";
+                                        }
+                                        ?>
+
+                                    </tbody>
+                                </table>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
